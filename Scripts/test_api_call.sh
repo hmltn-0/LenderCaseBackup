467e5f7195d3b6af546b4c1af76ce4bd1bc9ae83
Julius Hamilton <juliushamilton100@gmail.com>
2024-08-04 12:18:36 -0600

    Remove old script files after moving them to new directories

--- Scripts/test_api_call.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-openai api chat.completions.create -m gpt-3.5-turbo -g user "Say this is a test" --max-tokens 5
--- Scripts/test_text_recognition_in_images.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-openai api chat.completions.create -m gpt-4o-mini -g user "Extract text from this image: <IMAGE_URL>" --max-tokens 100
